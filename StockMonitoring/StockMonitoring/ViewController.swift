00c3e74193876edb906e30eb3b4c7a73a5b170d2
AMiracle-creator <65660523+AMiracle-creator@users.noreply.github.com>
2022-02-14 17:04:49 +0300

    feature/business-logic

--- StockMonitoring/StockMonitoring/ViewController.swift
+++ StockMonitoring/StockMonitoring/ViewController.swift
@@ -15,14 +15,18 @@ final class ViewController: UIViewController {
         static let iconTopAnchor: CGFloat = 64
         static let iconSizeMultiply: CGFloat = 0.1
         static let defaultInset: CGFloat = 16
+        static let activityIndicatorTopAnchor: CGFloat = 64
+
     }
+    
+    private let companies: [String: String] = ["Apple": "AAPL","Microsoft": "MSFT",
+                                               "Google": "GOOG", "Amazon": "AMZN", "Facebook": "FB"]
     
     // MARK: Private
 
     private let iconView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.image = #imageLiteral(resourceName: "alba.png")
         return imageView
     }()
     
@@ -36,7 +40,6 @@ final class ViewController: UIViewController {
     private let companyNameValueLabel: UILabel = {
         let companyNameValue = UILabel()
         companyNameValue.translatesAutoresizingMaskIntoConstraints = false
-        companyNameValue.text = "Apple"
         return companyNameValue
     }()
     
@@ -50,7 +53,6 @@ final class ViewController: UIViewController {
     private let symboValuelLabel: UILabel = {
         let symbolValue = UILabel()
         symbolValue.translatesAutoresizingMaskIntoConstraints = false
-        symbolValue.text = "AAPL"
         return symbolValue
     }()
     
@@ -64,7 +66,6 @@ final class ViewController: UIViewController {
     private let priceValueLabel: UILabel = {
         let priceValue = UILabel()
         priceValue.translatesAutoresizingMaskIntoConstraints = false
-        priceValue.text = "Price"
         return priceValue
     }()
     
@@ -78,7 +79,6 @@ final class ViewController: UIViewController {
     private let priceChangeValueLabel: UILabel = {
         let priceChangeValue = UILabel()
         priceChangeValue.translatesAutoresizingMaskIntoConstraints = false
-        priceChangeValue.text = "Price change value"
         return priceChangeValue
     }()
     
@@ -86,6 +86,12 @@ final class ViewController: UIViewController {
         let companyPicker = UIPickerView()
         companyPicker.translatesAutoresizingMaskIntoConstraints = false
         return companyPicker
+    }()
+    
+    private let activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        return activityIndicator
     }()
     
     // MARK: Lifecycle
@@ -96,6 +102,10 @@ final class ViewController: UIViewController {
         setupView()
         setupDelegates()
         activateConstraints()
+        
+        self.activityIndicator.hidesWhenStopped = true
+        
+        self.requestQuoteUpdate()
     }
 
 
@@ -118,6 +128,7 @@ final class ViewController: UIViewController {
         view.addSubview(priceChangeLabel)
         view.addSubview(priceChangeValueLabel)
         view.addSubview(priceValueLabel)
+        view.addSubview(activityIndicator)
         companyNameLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
         companyNameValueLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
     }
@@ -144,11 +155,125 @@ final class ViewController: UIViewController {
             priceChangeLabel.topAnchor.constraint(equalTo: priceLabel.bottomAnchor, constant: Constants.defaultInset),
             priceChangeValueLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.defaultInset),
             priceChangeValueLabel.topAnchor.constraint(equalTo: priceValueLabel.bottomAnchor, constant: Constants.defaultInset),
+            activityIndicator.topAnchor.constraint(equalTo: priceChangeLabel.bottomAnchor, constant: Constants.activityIndicatorTopAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             companyPicker.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -Constants.defaultInset),
             companyPicker.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             companyPicker.leadingAnchor.constraint(equalTo: view.leadingAnchor)
         ])
     }
+    
+    private func requestQuote(for symbol: String) {
+        let url = URL(string: "https://cloud.iexapis.com/stable/stock/\(symbol)/quote?&token=" + "pk_d723958d1ae7468a8a9b123180795903")!
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { data, response,  error in
+            guard
+               error == nil,
+               (response as? HTTPURLResponse)?.statusCode == 200,
+               let data = data
+            else {
+                print("Network error")
+                return
+            }
+
+            self.parseQuote(data: data)
+        }
+        
+        dataTask.resume()
+    }
+    
+    private func requestIcon(for symbol: String) {
+        let url = URL(string:  "https://cloud.iexapis.com/stable/stock/\(symbol)/logo?&token=pk_d723958d1ae7468a8a9b123180795903")!
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { data, response,  error in
+            guard
+               error == nil,
+               (response as? HTTPURLResponse)?.statusCode == 200,
+               let data = data
+            else {
+                print("Network error")
+                return
+            }
+            
+            self.downloadIcon(data: data)
+        }
+        
+        dataTask.resume()
+    }
+    
+    private func downloadIcon(data: Data) {
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: data)
+            
+            guard
+                let json = jsonObject as? [String: Any],
+                let url = json["url"] as? String,
+                let url = URL(string: url)
+            else {
+                print("Invalid JSON Format")
+                return
+            }
+            DispatchQueue.global().async {
+                let data = try? Data(contentsOf: url) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+                DispatchQueue.main.async {
+                    self.iconView.image = UIImage(data: data!)
+                    }
+            }
+            
+        } catch {
+            print("JSON parsing error: " + error.localizedDescription)
+
+        }
+    }
+    
+    private func parseQuote(data: Data) {
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: data)
+            
+            guard
+                let json = jsonObject as? [String: Any],
+                let companyName = json["companyName"] as? String,
+                let companySymbol = json["symbol"] as? String,
+                let price = json["latestPrice"] as? Double,
+                let priceChange = json["change"] as? Double
+            else {
+                print("Invalid JSON Format")
+                return
+            }
+            DispatchQueue.main.async {
+                self.displayStockInfo(companyName: companyName,
+                                      symbol: companySymbol,
+                                      price: price,
+                                      priceChange: priceChange)
+            }
+            
+            print("Company name is: '\(companyName)'")
+        } catch {
+            print("JSON parsing error: " + error.localizedDescription)
+        }
+    }
+    
+    private func displayStockInfo(companyName: String, symbol: String, price: Double, priceChange: Double) {
+        self.activityIndicator.stopAnimating()
+        self.companyNameValueLabel.text = companyName
+        self.symboValuelLabel.text = symbol
+        self.priceValueLabel.text = "\(price)"
+        self.priceChangeValueLabel.text = "\(priceChange)"
+    }
+    
+    private func requestQuoteUpdate() {
+        self.activityIndicator.startAnimating()
+        self.companyNameValueLabel.text = "-"
+        self.symboValuelLabel.text = "-"
+        self.priceValueLabel.text = "-"
+        self.priceChangeValueLabel.text = "-"
+        
+        let selectedRow = self.companyPicker.selectedRow(inComponent: 0)
+        let selectedSymbol = Array(self.companies.values)[selectedRow]
+        self.requestQuote(for: selectedSymbol)
+        self.requestIcon(for: selectedSymbol)
+    }
+    
 }
 
 extension ViewController: UIPickerViewDelegate {
@@ -161,10 +286,18 @@ extension ViewController: UIPickerViewDataSource {
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        3
+        self.companies.keys.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        "Test \(row)"
+        return Array(self.companies.keys)[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.activityIndicator.startAnimating()
+        
+        let selectedSymbol = Array(self.companies.values)[row]
+        self.requestQuote(for: selectedSymbol)
+        self.requestIcon(for: selectedSymbol)
     }
 }
